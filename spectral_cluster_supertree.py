e769d6861f9bc7c9eae33281e4da093f84064cd5
Robert McArthur <robert.mcarthur@anu.edu.au>
2023-07-24 17:47:30 +1000

    Make some methods semi-private. Add documentation

--- spectral_cluster_supertree.py
+++ spectral_cluster_supertree.py
@@ -1,46 +1,82 @@
 from typing import Iterable, Sequence, Optional, Set
-from cogent3.core.tree import TreeNode, TreeBuilder,PhyloNode
+from cogent3.core.tree import TreeNode, TreeBuilder, PhyloNode
 
 
 def spectral_cluster_supertree(
     trees: Sequence[TreeNode], weights: Optional[Sequence[float]]
 ) -> TreeNode:
-    """_summary_
+    """
+    Spectral Cluster Supertree (SCS).
+
+    Constructs a supertree from a collection of input trees. The supertree
+    method is inspired by Min-Cut Supertree (Semple & Steel, 2000), using
+    spectral clustering instead of min-cut to improve efficiency.
+
+    The set of input trees must overlap, the optional weights parameter
+    allows the biasing of some trees over others.
 
     Args:
-        trees (Iterable[TreeNode]): _description_
-        weights (Optional[float]): _description_
+        trees (Sequence[TreeNode]): Overlapping subtrees.
+        weights (Optional[Sequence[float]]): Optional weights for the trees.
 
     Returns:
-        TreeNode: _description_
+        TreeNode: The supertree containing all taxa in the input trees.
     """
 
     assert len(trees) >= 1, "there must be at least one tree"
 
+    # Input trees are of equal weight if none is specified
     if weights is None:
         weights = [1.0 for _ in range(len(trees))]
 
     assert len(trees) == len(weights), "trees and weights must be of same length"
 
-    all_names = get_all_tip_names(trees)
+    all_names = _get_all_tip_names(trees)
 
+    # If there are less than or only two names, can instantly return a tree
     if len(all_names) <= 2:
+        # TODO: if there is only one name, do I actually need to return
+        # A single tree node instead? Probably. Currently is root->single.
+        tree = _tip_names_to_tree(all_names)
+        return tree
 
+    # TODO: Construct proper cluster graph... perform spectral clustering
 
     return TreeNode()
 
 
-def get_all_tip_names(trees: Iterable[TreeNode]) -> Set:
+def _get_all_tip_names(trees: Iterable[TreeNode]) -> Set:
+    """
+    Fetch the tip names for some iterable of input trees.
+
+    Args:
+        trees (Iterable[TreeNode]): Input trees.
+
+    Returns:
+        Set: A set containing the tip names of the trees.
+    """
     names = set()
     for tree in trees:
         names.update(tree.get_tip_names())
     return names
 
-def tip_names_to_tree(tip_names: Iterable) -> TreeNode:
-    tree_builder = TreeBuilder(constructor=TreeNode).create_edge # Incorrectly causes "type error" on input TreeNode due to type detection system
+
+def _tip_names_to_tree(tip_names: Iterable) -> TreeNode:
+    """
+    Convert an iterable of tip names to a tree.
+    The tip names are made adjacent to a new root.
+
+    Args:
+        tip_names (Iterable): the names of the tips.
+
+    Returns:
+        TreeNode: A star tree with a root connecting each of the tip names
+    """
+    tree_builder = TreeBuilder(
+        constructor=TreeNode
+    ).create_edge  # Incorrectly causes "type error" on input TreeNode due to type detection system
     tips = [tree_builder([], tip_name, {}) for tip_name in tip_names]
-    tree = tree_builder(tips, "root", {}) # Might need to change "root" to something else, it is likely only temporarily the root.
+    tree = tree_builder(
+        tips, "root", {}
+    )  # Might need to change "root" to something else, it is likely only temporarily the root.
     return tree
-
-
-
