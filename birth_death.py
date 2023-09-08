2094e56de0fdb2fd3bdb5945251b83435022a7bd
Robert McArthur <robert.mcarthur@anu.edu.au>
2023-09-08 16:13:28 +1000

    Add documentation for the birth death tree

--- birth_death.py
+++ birth_death.py
@@ -12,15 +12,31 @@ import random
 def birth_death_tree(
     birth_rate: float, death_rate: float, stopping_time: float, restart_on_fail=False
 ) -> PhyloNode:
+    """
+    Generates a rooted phylogenetic tree according to the birth-death model given a birth rate,
+    death rate and stopping time (length of tips of generated tree from root).
+
+    If the birth-death model causes an extinction over the entire phylogeny, behavious is
+    dependent on restart_on_fail. If restart_on_fail is False, then raises a RuntimeException.
+    Otherwise, attempts to form a birth-death tree again.
+
+    Args:
+        birth_rate (float): The birth rate
+        death_rate (float): The death rate
+        stopping_time (float): Time for simulation
+        restart_on_fail (bool, optional): If True, total extinction restarts the process. Otherwise throws a RuntimeExeption. Defaults to False.
+
+    Raises:
+        RuntimeError: If the phylogeny entirely becomes extinct through the process. Can instead restart when restart_on_fail is True.
+
+    Returns:
+        PhyloNode: The resulting birth-death tree.
+    """
     total_rate = birth_rate + death_rate
     birth_probability = birth_rate / total_rate
 
     tree = make_tree("(t0:0.0,t1:0.0);")
-
     tips = list(tree.traverse(self_before=False, self_after=False))
-
-    # print(tips)
-    # print([tip.length for tip in tips])
 
     current_time = 0
     taxa_used = 2
@@ -74,28 +90,9 @@ def birth_death_tree(
                 grandparent.remove_node(parent)
                 grandparent.append(other_child)
 
-    # print(tree)
-    # print([tip.length for tip in tips])
-
-    # print(tree.ascii_art())
-
-    # lengths = []
-    # for tip in tree.iter_tips():
-    #     lengths.append(
-    #         sum(
-    #             map(
-    #                 lambda x: 0 if x.length is None else x.length,
-    #                 tip.ancestors() + [tip],
-    #             )
-    #         )
-    #     )
-
-    # print(lengths)
-
     return tree
 
 
 if __name__ == "__main__":
-    # random.seed(4)
     tree = birth_death_tree(0.8, 0.2, 1, True)
     print(tree.ascii_art())
