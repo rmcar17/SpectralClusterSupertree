7c2c5b5cf117d87f87af4607fc708a23411ce868
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-02-14 16:48:15 +1100

    add updated noxfile

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,4 @@
-import pathlib
-
+import os
 import nox
 
 
@@ -8,15 +7,9 @@ _py_versions = range(10, 12)
 
 @nox.session(python=[f"3.{v}" for v in _py_versions])
 def test(session):
-    py_version = session.python.replace(".", "")
-    session.install(".[test]")
-    session.install(".")
-    session.chdir("tests")
-    session.run(
-        "pytest",
-        "-s",
-        "-x",
-        "--cov-report",
-        f"lcov:lcov-{session.python}.info",
-        "--cov",
-    )
+    posargs = list(session.posargs)
+    env = os.environ.copy()
+
+    install_spec = "-e.[test]"
+    session.install(install_spec)
+    session.run("pytest", *posargs, env=env)
