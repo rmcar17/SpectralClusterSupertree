67a9b65fb9eb07a2df3ceeeecd1fba9ee577fbc8
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-02-15 12:35:07 +1100

    Slightly increase coverage

--- tests/test_spectral_cluster_supertree.py
+++ tests/test_spectral_cluster_supertree.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import pytest
 from cogent3 import make_tree
 from spectral_cluster_supertree import spectral_cluster_supertree
+from spectral_cluster_supertree.scs.scs import tuple_sorted
 
 
 def scs_test(in_trees, expected):
@@ -25,7 +26,10 @@ def test_agreeable():
 
     tree_1 = make_tree("(((a,b),(c,d)),(z,(x,y)))")
     tree_2 = make_tree("((a,((f,g),b)),(c,(d,e)))")
-
+    
     expected = make_tree("(((a,(b,(f,g))),(c,(d,e))),((x,y),z))")
 
     scs_test([tree_1, tree_2], expected)
+
+def test_tuple_sorted():
+    assert tuple_sorted([3,1,2]) == (1,2,3)
