ee59435e10c00e177d4ae5e87f814698d2e7ec69
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-02-26 10:45:33 +1100

    Add more historical results

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -216,3 +216,17 @@ def test_dcm_iq():
     source_trees = load_source_tree_file("dcm_iq_source.tre")
 
     scs_test_cli(source_trees, expected, pcg_weighting="branch")
+
+
+def test_smidgenog():
+    expected = load_expected_tree_file("smidgenog_expected.tre")
+    source_trees = load_source_tree_file("smidgenog_source.tre")
+
+    scs_test_cli(source_trees, expected, pcg_weighting="branch")
+
+
+def test_supertriplets():
+    expected = load_expected_tree_file("supertriplets_expected.tre")
+    source_trees = load_source_tree_file("supertriplets_source.tre")
+
+    scs_test_cli(source_trees, expected, pcg_weighting="depth")
