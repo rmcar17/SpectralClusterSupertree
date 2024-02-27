d799938f75040a5f5bf697656ccded319c48b99a
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-02-28 10:45:26 +1100

    add docstring to load_source_trees

--- src/spectral_cluster_supertree/__init__.py
+++ src/spectral_cluster_supertree/__init__.py
@@ -13,12 +13,26 @@ __version__ = "2024.02.26"
 __maintainer__ = "Robert McArthur"
 __status__ = "Production"
 
+import os
+
 from cogent3 import TreeNode, make_tree
 
 from spectral_cluster_supertree.scs import spectral_cluster_supertree
 
 
-def load_source_trees(source_tree_file: str) -> list[TreeNode]:
+def load_source_trees(source_tree_file: str | bytes | os.PathLike) -> list[TreeNode]:
+    """Load a line-separated file of Newick-formatted trees.
+
+    Parameters
+    ----------
+    source_tree_file : str | bytes | os.PathLike
+        The path to the source tree file.
+
+    Returns
+    -------
+    list[TreeNode]
+        A list of all source trees in the file.
+    """
     with open(source_tree_file, "r") as f:
         source_trees: list[TreeNode] = [make_tree(line.strip()) for line in f]  # type: ignore
     return source_trees
