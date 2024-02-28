fea20a794bcc75c7b8a7c571fc5f9e54e55b0ea0
Robert McArthur <robert.mcarthur@anu.edu.au>
2024-02-29 10:37:58 +1100

    update version

--- src/sc_supertree/__init__.py
+++ src/sc_supertree/__init__.py
@@ -7,7 +7,7 @@ A scalable and accurate algorithm for merging rooted phylogenetic trees.
 __all__ = ["load_trees", "construct_supertree"]
 __copyright__ = "Copyright 2023, Robert McArthur"
 __license__ = "BSD"
-__version__ = "2024.2.28.a1"
+__version__ = "2024.2.29"
 
 import os
 
