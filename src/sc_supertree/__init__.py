e2d624193e1ba774b5b7e92ea69af445eec44d7c
Robert McArthur <robert.mcarthur@anu.edu.au>
2026-05-15 17:06:06 +1000

    REL: Bump version

--- src/sc_supertree/__init__.py
+++ src/sc_supertree/__init__.py
@@ -9,4 +9,4 @@ from sc_supertree.scs import construct_supertree
 __all__ = ["construct_supertree", "load_trees"]
 __copyright__ = "Copyright 2023, Robert McArthur"
 __license__ = "BSD"
-__version__ = "2025.12.9"
+__version__ = "2026.5.15"
